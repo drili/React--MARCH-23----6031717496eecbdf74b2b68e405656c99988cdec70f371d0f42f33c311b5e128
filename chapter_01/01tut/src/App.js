d679d54d2b87026a67e8ff0c02c02458d566e1e2
drili <DriliB@hotmail.com>
2023-03-27 19:17:42 +0200

    🔨 chapter 11 [DONE]

--- chapter_01/01tut/src/App.js
+++ chapter_01/01tut/src/App.js
@@ -3,24 +3,23 @@ import Content from './Content'
 import Footer from './Footer'
 import AddItem from './AddItem'
 import SearchItem from './SearchItem'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")))
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || [])
     const [newItem, setNewItem] = useState("")
     const [search, setSearch] = useState("")
 
-    const setAndSaveItems = (newItems) => {
-        setItems(newItems)
-        localStorage.setItem("shoppinglist", JSON.stringify(newItems))
-    }
+    useEffect(() => {
+        localStorage.setItem("shoppinglist", JSON.stringify(items))
+    }, [items])
 
     const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1
         const myNewItem = { id, checked: false, item }
         const listItems = [...items, myNewItem]
 
-        setAndSaveItems(listItems)
+        setItems(listItems)
     }
 
     const handleCheck = (id) => {
@@ -30,13 +29,13 @@ function App() {
                 : item
         )
 
-        setAndSaveItems(listItems)
+        setItems(listItems)
     }
 
     const handleDelete = (id) => {
         const listItems = items.filter((item) => item.id !== id)
 
-        setAndSaveItems(listItems)
+        setItems(listItems)
     }
 
     const handleSubmit = (e) => {
